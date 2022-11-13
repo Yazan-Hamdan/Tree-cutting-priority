a8a5999a8ee989912706d0a288bb6ed6de6cb78d
yazanhamdan591 <yazanalatrash591@gmail.com>
2022-11-13 12:43:36 +0200

    linting issues, fixed reading functionality

--- utils/shape_files_directory_handler.py
+++ utils/shape_files_directory_handler.py
@@ -7,18 +7,37 @@ class ShapeFilesDirectoryHandler:
     def __init__(self, directory_name: str):
         """
         Args:
-            directory_name:[str] The name of directory where shapefiles are stored
+            directory_name:[str] The name of directory where shapefiles are
+                                 stored
         """
         # get path of the base directory
         base_dir_path = os.path.normpath(os.getcwd() + os.sep + os.pardir)
         # join base_dir_path with the data_directory
-        self.__directory_path = os.path.join(base_dir_path, f'{directory_name}')
+        self.__directory_path = os.path.join(base_dir_path,
+                                             f'{directory_name}')
 
     def __get_shapefiles(self) -> list:
-        shape_files = [f for f in os.listdir(self.__directory_path) if f.endswith('.shp')]
+        """
+        return all shapefiles of a directory
+        Returns:
+            shape_files: [list] all shapefiles into one list
+        """
+        shape_files = [f for f in os.listdir(self.__directory_path) if
+                       f.endswith('.shp')]
         return shape_files
 
     def read_shapefiles(self) -> dict:
+        """
+        open a list of shapefiles into one list
+
+        Returns: files_data: [dict] where key is the name of the file,
+                                    value is the data stored in that file
+
+        Note:
+
+        """
         shape_files = self.__get_shapefiles()
-        files_data = {filename: ogr.Open(filename) for filename in shape_files}
+        files_data = {
+            filename: ogr.Open(os.path.join(self.__directory_path, filename))
+            for filename in shape_files}
         return files_data
