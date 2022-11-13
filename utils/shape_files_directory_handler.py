54fd9f625b370114269de10b4fbdb61e4a05d872
yazanhamdan591 <yazanalatrash591@gmail.com>
2022-11-13 12:06:38 +0200

    parameter name change

--- utils/shape_files_directory_handler.py
+++ utils/shape_files_directory_handler.py
@@ -4,17 +4,21 @@ from osgeo import ogr
 
 
 class ShapeFilesDirectoryHandler:
-    def __init__(self, directory_path: str):
+    def __init__(self, directory_name: str):
+        """
+        Args:
+            directory_name:[str] The name of directory where shapefiles are stored
+        """
         # get path of the base directory
         base_dir_path = os.path.normpath(os.getcwd() + os.sep + os.pardir)
         # join base_dir_path with the data_directory
-        self.__directory_path = os.path.join(base_dir_path, f'{directory_path}')
+        self.__directory_path = os.path.join(base_dir_path, f'{directory_name}')
 
     def __get_shapefiles(self) -> list:
         shape_files = [f for f in os.listdir(self.__directory_path) if f.endswith('.shp')]
         return shape_files
 
-    def read_shapefiles(self):
+    def read_shapefiles(self) -> dict:
         shape_files = self.__get_shapefiles()
         files_data = {filename: ogr.Open(filename) for filename in shape_files}
         return files_data
